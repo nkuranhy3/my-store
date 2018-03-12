3c976d0da249338ec5928d4e3ffb9cb68e48ec62
Alfred Boateng <programmingwithalfred@gmail.com>
2018-03-12 06:20:49 +0000

    boostrap styling added

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ var routes = require("./models/routes");
 mongoose.connect("mongodb://localhost/store");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 
 app.use(routes);
 
 
 app.listen(process.env.PORT, process.env.IP, function () {
-    console.log("server has strted");
+    console.log("APP HAS STARTED ON PORT " + process.env.PORT);
 });
